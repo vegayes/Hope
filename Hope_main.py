31fb98fc476c76a0f0fe8b2bfa8edea39eaa6de8
constmine <uusstt069@naver.com>
2023-01-22 22:24:01 +0900

    Change Class

--- Hope_main.py
+++ Hope_main.py
@@ -1,82 +1,36 @@
-import glob
-import sys
 import time
 
 import serial
-from PyQt5.QtWidgets import *
 from PyQt5 import QtWidgets
 
 import Hope_UI
-from Hope_UI import Ui_HOPE
+
 
 class CannotConnectException(Exception):
-	pass
-
+    pass
 
 
 class UI_Window(QtWidgets.QMainWindow, Hope_UI.Ui_HOPE):
     def __init__(self):
+        from tools.Hope_Port import port_hope
         super(UI_Window, self).__init__()
+
         self.ui = Hope_UI.Ui_HOPE()
         self.ui.setupUi(self)
 
-        self.populate_ports()
-        self.populate_microstepping()
-        self.set_port()
+        self.ports = port_hope(self, self.ui)
 
-        self.ui.refresh_ports_button.clicked.connect(self.refresh_ports)
+        self.ports.populate_ports()
+        self.populate_microstepping()
+        self.ports.set_port()
+
+        self.ui.refresh_ports_button.clicked.connect(self.ports.refresh_ports)
         self.ui.connect_button.clicked.connect(self.connect)
 
         # Declaring start, mid, and end marker for sending code to Arduino / Arduino에 코드를 보내기 위한 시작, 중간 및 끝 마커 선언
         self.startMarker = 60  # <
         self.endMarker = 62  # ,F,0.0>
         self.midMarker = 44  # ,
-
-
-        #이것은 예제입니다.
-
-
-
-    # Populate the available ports
-    def populate_ports(self):
-        """
-            :raises EnvironmentError:
-                On unsupported or unknown platforms
-            :returns:
-                A list of the serial ports available on the system
-        """
-        print("Serial Port 찾는 중... ")
-        if sys.platform.startswith('win'):
-            ports = ['COM%s' % (i + 1) for i in range(256)]
-        elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
-            # this excludes your current terminal "/dev/tty"
-            ports = glob.glob('/dev/tty[A-Za-z]*')
-        elif sys.platform.startswith('darwin'):
-            ports = glob.glob('/dev/tty.*')
-        else:
-            raise EnvironmentError('Unsupported platform')
-
-        result = []
-        for port in ports:
-            try:
-                s = serial.Serial(port)
-                s.close()
-                result.append(port)
-            except (OSError, serial.SerialException):
-                pass
-        self.ui.serial_port_combo.addItems(result)
-        print(f"{result} 발견됨")
-
-        # Refresh the list of ports
-
-    def refresh_ports(self):
-        self.statusBar().showMessage("You clicked REFRESH PORTS")
-        self.ui.serial_port_combo.clear()
-        self.populate_ports()
-        self.set_port()
-
-    def set_port(self):
-        self.port = self.ui.serial_port_combo.currentText()
 
     def populate_microstepping(self):
         self.microstepping_values = ['1', '2', '4', '8', '16', '32']
@@ -106,9 +60,9 @@ class UI_Window(QtWidgets.QMainWindow, Hope_UI.Ui_HOPE):
                 # self.global_listener_thread.finished.connect(lambda:self.self.thread_finished(self.global_listener_thread))
                 # self.global_listener_thread.start()
 
-                # ~~~~~~~~~~~~~~~~
+                # ~~~~
                 # TAB : Setup
-                # ~~~~~~~~~~~~~~~~
+                # ~~~~
                 # self.ui.disconnect_BTN.setEnabled(True) 연결 끊기
 
                 self.ui.connect_button.setEnabled(False)
@@ -120,7 +74,7 @@ class UI_Window(QtWidgets.QMainWindow, Hope_UI.Ui_HOPE):
         except AttributeError:
             self.statusBar().showMessage("보드를 연결 시킨 후 정확하게 포트와 연결하세요.")
 
-    def recvPositionArduino(self):    #distance 기능 ??
+    def recvPositionArduino(self):  # distance 기능 ??
         startMarker = self.startMarker
         midMarker = self.midMarker
         endMarker = self.endMarker
@@ -148,6 +102,7 @@ class UI_Window(QtWidgets.QMainWindow, Hope_UI.Ui_HOPE):
 
         return (ck)
 
+
 def main():
     import sys
     app = QtWidgets.QApplication(sys.argv)
@@ -155,11 +110,11 @@ def main():
     Window.setWindowTitle("Poseidon Pumps Controller - Pachter Lab Caltech 2018")
     Window.show()
     # HOPE = QtWidgets.QDialog()
-    # ui = Ui_HOPE()
+    # ui = UiHOPE()
     # ui.setupUi(HOPE)
     # HOPE.show()
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
 
 
-if __name__ == "__main__":
+if __name__ == "__main__" :
     main()
